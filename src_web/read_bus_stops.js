438363b18db7e40c75615c3740ac9c9e49d431a3
Noce99 <enrico.mannocci99@gmail.com>
2024-10-29 15:33:53 +0100

    Now the json file is loaded just one time ad the zoom is centered!

--- src_web/read_bus_stops.js
+++ src_web/read_bus_stops.js
@@ -1,8 +1,14 @@
 let canvas;
 let ctx;
 let zoom_value = 100;
+let data;
 let translation_x = 0;
 let translation_y = 0;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchJSONData();
+});
 
 function fetchJSONData() {
     canvas = document.getElementById("myCanvas");
@@ -13,52 +19,67 @@ function fetchJSONData() {
                 throw new Error(`HTTP error! Status: ${res.status}`);
         }
         return res.json();
-    }).then((data) =>
-        draw_data(data)
-    ).catch((error) =>
+    }).then((d) => {
+        data=d;
+        draw_data();
+    }).catch((error) =>
         console.error("Unable to fetch data:", error)
     );
 }
 
-function draw_data(data){
+function draw_data(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i=0; i < data.length; i++) {
         ctx.beginPath();
-        ctx.arc(parseInt(data[i].x)/zoom_value + translation_x,
-                parseInt(data[i].y)/zoom_value + translation_y,
+        ctx.arc((parseInt(data[i].x) + translation_x)/zoom_value + canvas.width/2,
+            (parseInt(data[i].y) + translation_y)/zoom_value + canvas.height/2,
             2, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fillStyle = 'rgba(255,0,0,1.0)';
         ctx.fill();
     }
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2-0.01*canvas.width, canvas.height/2);
+    ctx.lineTo(canvas.width/2+0.01*canvas.width, canvas.height/2);
+    ctx.moveTo(canvas.width/2, canvas.height/2-0.01*canvas.height);
+    ctx.lineTo(canvas.width/2, canvas.height/2+0.01*canvas.height);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 3;
+    ctx.stroke();
 }
 
 function zoom(){
     zoom_value -= 10;
-    fetchJSONData();
+    if (zoom_value <= 10) {
+        zoom_value = 10
+    }
+    draw_data();
 }
 
 function unzoom(){
     zoom_value += 10;
-    fetchJSONData();
+    if (zoom_value >= 200) {
+        zoom_value = 200
+    }
+    draw_data();
 }
 
 function right(){
-    translation_x -= 40;
-    fetchJSONData();
+    translation_x -= 40*zoom_value;
+    draw_data();
 }
 
 function left(){
-    translation_x += 40;
-    fetchJSONData();
+    translation_x += 40*zoom_value;
+    draw_data();
 }
 
 function up(){
-    translation_y += 40;
-    fetchJSONData();
+    translation_y += 40*zoom_value;
+    draw_data();
 }
 
 function down(){
-    translation_y -= 40;
-    fetchJSONData();
+    translation_y -= 40*zoom_value;
+    draw_data();
 }
