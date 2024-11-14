1a791eb0335823c4e60100dad95e6b53423c1e8e
Noce99 <enrico.mannocci99@gmail.com>
2024-11-14 16:03:49 +0100

    Started implementing live gps!

---
+++ src_server/get_info_gps.py
@@ -0,0 +1,38 @@
+import socket
+
+
+class GpsLivelox:
+    def __init__(self):
+        host = "0.0.0.0"
+        port = 10999
+
+        self.server_socket = socket.socket()
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((host, port))
+        self.lon = None
+        self.lat = None
+
+    def listen_forever(self):
+        while True:
+            self.server_socket.listen(2)
+            conn, address = self.server_socket.accept()
+            print("Connection from: " + str(address))
+            conn.recv(1280)
+            message = '\x01'
+            message = message.encode('utf-8')
+            conn.send(message)
+            data = conn.recv(1280)
+            self.lat = self.coordinate_formater(data.hex()[38:46])
+            self.lon = self.coordinate_formater(data.hex()[46:54])
+            print(f"{self.lat} N  {self.lon} E")
+            conn.close()
+
+    @staticmethod
+    def coordinate_formater(hex_coordinate):
+        coordinate = int(hex_coordinate, 16)
+        if coordinate & (1 << 31):
+            new_int = coordinate - 2 ** 32
+            dec_coordinate = new_int / 1e7
+        else:
+            dec_coordinate = coordinate / 10000000
+        return dec_coordinate
--- src_server/server.py
+++ src_server/server.py
@@ -1,19 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from get_info_gps import GpsLivelox
 app = Flask(__name__)
-CORS(app)  # Permette richieste da qualsiasi dominio
+CORS(app)
+
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {'message': 'Ciao dal server Python!'}
     return jsonify(data)
 
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'message': 'Ciao dal server Python!'}
+    return jsonify(data)
+
+
 @app.route('/api/data', methods=['POST'])
 def post_data():
     content = request.json
     response = {'received': content}
     return jsonify(response)
+
 
 @app.route('/login', methods=['POST'])
 def post_login():
@@ -22,5 +31,9 @@ def post_login():
     response = {'received': content}
     return jsonify(response)
 
+
 if __name__ == '__main__':
-    app.run(port=4989)
+    # app.run(port=4989)
+    my_gps = GpsLivelox()
+    my_gps.listen_forever()
+    
