659178e8cb7a6b9a266d808b335e2c57e01223a9
Noce99 <enrico.mannocci99@gmail.com>
2024-11-24 09:02:09 +0100

    Removed bus stops outside the map!

--- process_functions.py
+++ process_functions.py
@@ -1,6 +1,20 @@
 import csv
 import json
 
+stops_to_remove = ["MONTE ALBANO 11","BASILICA DI SAN LUCA","SAN LUCA 35","SAN LUCA 15","CASAGLIA SCUOLE",
+                   "RAVONE","CASAGLIA 32-34","CASAGLIA 31","SCUOLE CARRACCI","CASAGLIA","OSSERVANZA","VILLA ALDINI",
+                   "BIVIO COLLI","SAN MAMOLO","VILLA GHIGI","BAGNI DI MARIO","VILLA BARUZZIANA","SAN MICHELE IN BOSCO",
+                   "ISTITUTO ORTOPEDICO RIZZO","PIAZZALE BACCHELLI","VILLA REVEDIN","SCALINI","BARBIANO","BARBIANO 1",
+                   "IST. RIZZOLI - CENTRO RIC","POLIAMBULATORIO RIZZOLI","VILLA COLONNA","CERETOLO CHIESA","CERETOLO",
+                   "Z.I. CASAL. PARINI","Z.I. CASAL. BIZZARRI","COLLEGIO SAN LUIGI@","BOIARDO14@","VILLA HERCOLANI",
+                   "MOLINELLI","BORGHI MAMO","RAGNO","MEZZOFANTI","STERLINO","SAVIOLI","ALBERTONI",
+                   "PIAZZA TRENTO TRIESTE","ALEMANNI - AGEOP RICERCA","OSPEDALE SANT`ORSOLA ALBE","BENTIVOGLI",
+                   "CIRENAICA","MASIA","CAVALCAVIA SAN DONATO","ROSSI","MERCATO SAN DONATO","GALEOTTI","REPUBBLICA",
+                   "ZACCONI","PEZZANA","RUGGERI","LAVORO","NOVELLI","SERENA","ALDO MORO","FIERA ALDO MORO","VESTRI",
+                   "CAVALCAVIA MASCARELLA","STALINGRADO","DOPOLAVORO FERROVIARIO","BIGARI","JACOPO DELLA QUERCIA",
+                   "MATTEOTTI ALTA VELOCITA`","CARRACCI STAZIONE AV","SACRO CUORE","TIARINI",
+                   "FIORAVANTI PIAZZA LIBER P","CASA DELLA SALUTE UFF COM","CASA DELLA SALUTE NAVILE",
+                   "CARRACCI","CASE FERROVIERI"]
 
 def from_csv_to_json(csv_path, json_path):
     pass
@@ -14,7 +28,8 @@ def process_bus_stops(csv_input_path, json_output_path):
         for bus_stop in csvFile:
             lat = float(bus_stop[6].replace(",", "."))
             lon = float(bus_stop[7].replace(",", "."))
-            if 44.47267601010103 < lat < 44.50946314343433 and 11.256635603112843 < lon < 11.365479808365757:
+            if 44.47267601010103 < lat < 44.50946314343433 and 11.256635603112843 < lon < 11.365479808365757 and \
+                bus_stop[1] not in stops_to_remove:
                 bus_stops.append(
                     {"name": bus_stop[1], "verbal_location": bus_stop[2],
                      "x": int(bus_stop[4]), "y": int(bus_stop[5]),
